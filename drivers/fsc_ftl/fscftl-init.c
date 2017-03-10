af187270a7411646b7cc353cc8ddd44f9fc0e2c8
Dengcai Xu <xudc14@163.com>
2017-03-11 01:57:59 +0800

    fscftl Module init/exit psudo code TBD

---
+++ drivers/fsc_ftl/fscftl-init.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2017 Group XX
+ * Initial release: 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * fscftl initialization.
+ */
+
+static int __init fscftl_module_init(void)
+{
+	return 0;
+}
+
+static void fscftl_module_exit(void)
+{
+	return;
+}
+
+module_init(fscftl_module_init);
+module_exit(fscftl_module_exit);
+MODULE_AUTHOR("Dengcai Xu <dxu@cnexlabs.com>");
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("Host-Based Full Stack Control FTL for NVMe SSDs");
+
